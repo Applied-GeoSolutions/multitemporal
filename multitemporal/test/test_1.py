b27ac8bb5643c810593715a7189b5112e1850c4d
Bobby H Braswell <rbraswell@ags.io>
2018-03-12 17:07:38 +0000

    test1 passes

---
+++ multitemporal/test/test_1.py
@@ -0,0 +1,21 @@
+import os
+import json
+from multitemporal.mt import run
+from osgeo import gdal
+
+CWD = os.path.dirname(__file__)
+TESTDATA = os.path.join(CWD, 'test1')
+TESTCONF = os.path.join(CWD, 'test1.json')
+
+def test():
+    args = json.loads(open(TESTCONF).read())
+    args['projname'] = 'test1cmp'
+    args['projdir'] = TESTDATA
+    args['outdir'] = args['projdir'] + 'out'
+    run(**args)
+    outfile1 = os.path.join(args['outdir'], 'test1_correlate.tif')
+    outfile1cmp = os.path.join(args['outdir'], 'test1cmp_correlate.tif')
+    x1 = gdal.Open(outfile1).ReadAsArray()
+    x1cmp = gdal.Open(outfile1cmp).ReadAsArray()
+    assert (x1 - x1cmp).sum() < 0.0001
+    print "pass"
