220586cc2932f7ec4d83245cb4b746e5f2ec6c95
Tom Olson <tolson@appliedgeosolutions.com>
2019-10-08 14:33:27 -0400

    43 passthrough test runs to completion (& fails)

--- multitemporal/test/test_1.py
+++ multitemporal/test/test_1.py
@@ -7,6 +7,7 @@ CWD = os.path.dirname(__file__)
 TESTDATA = os.path.join(CWD, 'test1')
 TESTCONF = os.path.join(CWD, 'test_correlate.json')
 
+
 def test_correlate():
     if not os.path.exists(TESTDATA):
         os.system('tar xfz {}.tgz -C {}'.format(TESTDATA, os.path.split(TESTDATA)[0]))
@@ -17,6 +18,27 @@ def test_correlate():
     run(**args)
     outfile1 = os.path.join(args['outdir'], 'test1_correlate.tif')
     outfile1cmp = os.path.join(args['outdir'], 'test1cmp_correlate.tif')
+    x1 = gdal.Open(outfile1).ReadAsArray()
+    x1cmp = gdal.Open(outfile1cmp).ReadAsArray()
+    assert (x1 - x1cmp).sum() < 0.0001
+
+
+def test_passthrough():
+    """Use the passthrough module as a way to test mt throughput."""
+    TESTDATA = os.path.join(CWD, 'test1')
+    TESTCONF = os.path.join(CWD, 'test_passthrough.json')
+    if not os.path.exists(TESTDATA):
+        os.system('tar xfz {}.tgz -C {}'.format(TESTDATA, os.path.split(TESTDATA)[0]))
+    args = json.loads(open(TESTCONF).read())
+    # ./multitemporal/test/test1out/test1cmp_passthrough_2014.tif
+    # ./multitemporal/test/test1out/test1cmp_passthrough_2015.tif
+    # ./multitemporal/test/test1out/test1cmp_passthrough_2016.tif
+    args['projname'] = 'test1cmp'
+    args['projdir'] = TESTDATA
+    args['outdir'] = args['projdir'] + 'out'
+    run(**args)
+    outfile1 = os.path.join(args['outdir'], 'test1_passthrough.tif')
+    outfile1cmp = os.path.join(args['outdir'], 'test1cmp_passthrough.tif')
     x1 = gdal.Open(outfile1).ReadAsArray()
     x1cmp = gdal.Open(outfile1cmp).ReadAsArray()
     assert (x1 - x1cmp).sum() < 0.0001
