5b2d0f7459f170e4c3bd9d21bc9c1dd4b9f69453
Tom Olson <tolson@appliedgeosolutions.com>
2019-10-11 13:43:48 -0400

    43 cleanup, set version

--- multitemporal/test/test_1.py
+++ multitemporal/test/test_1.py
@@ -11,27 +11,6 @@ from multitemporal import mt
 def data_dir():
     return os.path.join(os.path.dirname(__file__), 'data')
 
-CWD = os.path.dirname(__file__)
-TESTDATA = os.path.join(CWD, 'test1')
-TESTCONF = os.path.join(CWD, 'test_correlate.json')
-
-
-# for some reason pytest.mark.skip isn't working:
-# @pytest.mark.skip(reason='no worky due to neglect it seems')
-# def test_correlate():
-#     if not os.path.exists(TESTDATA):
-#         os.system('tar xfz {}.tgz -C {}'.format(TESTDATA, os.path.split(TESTDATA)[0]))
-#     args = json.loads(open(TESTCONF).read())
-#     args['projname'] = 'test1cmp'
-#     args['projdir'] = TESTDATA
-#     args['outdir'] = args['projdir'] + 'out'
-#     mt.run(**args)
-#     outfile1 = os.path.join(args['outdir'], 'test1_correlate.tif')
-#     outfile1cmp = os.path.join(args['outdir'], 'test1cmp_correlate.tif')
-#     x1 = gdal.Open(outfile1).ReadAsArray()
-#     x1cmp = gdal.Open(outfile1cmp).ReadAsArray()
-#     assert (x1 - x1cmp).sum() < 0.0001
-
 
 test_passthrough_args = {
     'compthresh': 0.01, # so smaller dataset will work
